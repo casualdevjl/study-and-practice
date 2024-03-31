0ce86335d4a8ac387902e2375cc6c834d7e7437a
casualdevjl <devjlumba@gmail.com>
2024-03-31 22:37:25 +0800

    Practicing email validation

--- form validation practice/scripts.js
+++ form validation practice/scripts.js
@@ -3,20 +3,34 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
-const errorMessage = document.getElementById('error-message');
+// const defaultErrorMessage = document.getElementById('error-message');
 
+// defaultErrorMessage.textContent = "";
 
 // Check username
 function checkUsername(whateveriwant) {
-    const formControl = username.parentElement;
+    const formControl = whateveriwant.parentElement;
 
-    if (username.value === "" || username.value.length < 4) {
+    if (whateveriwant.value === "" || whateveriwant.value.length < 4) {
         // Show error
         formControl.className = "form-control error";
-        const errorParagraph = form.querySelector('small');
-        errorParagraph.innerText = "Username must be at least 4 characters long";
+        const smallUsername = form.querySelector('#error-message-username');
+        smallUsername.innerText = "Username must be at least 4 characters long";
     } else {
         // Success green border
+        formControl.className = "form-control success";
+    }
+}
+
+function checkEmail(couldbeanything) {
+    const formControl = couldbeanything.parentElement;
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,6}$/;
+
+    if (couldbeanything.value === "" || !emailPattern.test(couldbeanything.value)) {
+        formControl.className = "form-control error";
+        const smallEmail = form.querySelector('#error-message-email');
+        smallEmail.innerText = "Please enter a valid email address";
+    } else {
         formControl.className = "form-control success";
     }
 }
@@ -25,4 +39,5 @@ function checkUsername(whateveriwant) {
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     checkUsername(username);
+    checkEmail(email);
 });
